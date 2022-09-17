a2c999c65047826ac6468363cdc0f2bf9889223e
shiklax <radek32411@gmail.com>
2022-09-17 16:49:25 +0200

    added changeing color buttons

--- BattleShips/MainWindow.xaml.cs
+++ BattleShips/MainWindow.xaml.cs
@@ -18,6 +18,9 @@ namespace BattleShips {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        List<Button> player1List = new List<Button>();
+        List<Button> player2List = new List<Button>();
+        bool rotateVertical = true;
         public MainWindow() {
             InitializeComponent();
         }
@@ -25,20 +28,59 @@ namespace BattleShips {
         private void StartGameClick(object sender, RoutedEventArgs e) {            
             for (int i = 0; i < 10; i++) {
                 for (int j = 0; j < 10; j++) {
-                    Button b1 = new Button() {Background = Brushes.Black};
-                    Button b2 = new Button() {Background = Brushes.Black};
-                    b1.Style = (Style)FindResource("btnStyle");
-                    b2.Style = (Style)FindResource("btnStyle");
+                    Button b1 = new Button() {Background = Brushes.DarkBlue};
+                    Button b2 = new Button() {Background = Brushes.DarkBlue };
+                    b1.Click += B1_Click;
+                    b1.MouseEnter += B1_MouseEnter;
+                    b1.MouseLeave += B1_MouseLeave;
+                    b2.Click += B2_Click;
+                    b2.MouseEnter += B2_MouseEnter;
                     Grid.SetRow(b1, i);
                     Grid.SetColumn(b1, j);
                     Grid.SetRow(b2, i);
                     Grid.SetColumn(b2, j);
                     Player1Grid.Children.Add(b1);
                     Player2Grid.Children.Add(b2);
+                    player1List.Add(b1);
+                    player2List.Add(b2);
                 }
             }
-            Player1Grid.Background = Brushes.Black;
-            Player2Grid.Background = Brushes.Black;
+            StartGameButton.IsEnabled = false;
+            RotateButton.IsEnabled = true;
+        }
+
+        private void B1_MouseLeave(object sender, MouseEventArgs e) {
+            int i = player1List.IndexOf((Button)e.Source);
+            player1List[i].Background = Brushes.DarkBlue;
+        }
+
+        private void B2_MouseEnter(object sender, MouseEventArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private void B1_MouseEnter(object sender, MouseEventArgs e) {
+            int i = player1List.IndexOf((Button)e.Source);
+            if (rotateVertical) {
+                player1List[i + 10].Background = Brushes.Yellow;
+                player1List[i + 20].Background = Brushes.Yellow;
+
+
+            } else {
+                player1List[i + 1].Background = Brushes.Yellow;
+                player1List[i + 2].Background = Brushes.Yellow;
+            }
+        }
+
+        private void B2_Click(object sender, RoutedEventArgs e) {
+            
+        }
+
+        private void B1_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void RotateButtonClick(object sender, RoutedEventArgs e) {
+            rotateVertical = !rotateVertical;
         }
     }
 }

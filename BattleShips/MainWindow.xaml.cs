58b39aa3b0377e792a94691d26cdb94f3d5cccfa
shiklax <radek32411@gmail.com>
2022-09-17 12:45:14 +0200

    draw board update

--- BattleShips/MainWindow.xaml.cs
+++ BattleShips/MainWindow.xaml.cs
@@ -21,5 +21,20 @@ namespace BattleShips {
         public MainWindow() {
             InitializeComponent();
         }
+
+        private void StartGameClick(object sender, RoutedEventArgs e) {            
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    Button b1 = new Button() {Background = Brushes.Black };
+                    Button b2 = new Button() {Background = Brushes.Black,};
+                    Grid.SetRow(b1, i);
+                    Grid.SetColumn(b1, j);
+                    Grid.SetRow(b2, i);
+                    Grid.SetColumn(b2, j);
+                    Player1Grid.Children.Add(b1);
+                    Player2Grid.Children.Add(b2);
+                }
+            }
+        }
     }
 }

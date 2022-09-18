67ffc005ec48d35abb2c04d7be56b4bd1a8cb0c5
shiklax <radek32411@gmail.com>
2022-09-18 14:21:09 +0200

    update

--- BattleShips/MainWindow.xaml.cs
+++ BattleShips/MainWindow.xaml.cs
@@ -21,15 +21,22 @@ namespace BattleShips {
         List<Button> player1List = new List<Button>();
         List<Button> player2List = new List<Button>();
         bool rotateVertical = true;
+        int maxValue = 99;
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int ah = 1;
+        int bh = 2;
+        int ch = 3;
         public MainWindow() {
             InitializeComponent();
         }
 
-        private void StartGameClick(object sender, RoutedEventArgs e) {            
+        private void StartGameClick(object sender, RoutedEventArgs e) {
             for (int i = 0; i < 10; i++) {
                 for (int j = 0; j < 10; j++) {
-                    Button b1 = new Button() {Background = Brushes.DarkBlue};
-                    Button b2 = new Button() {Background = Brushes.DarkBlue };
+                    Button b1 = new Button() { Background = Brushes.DarkBlue };
+                    Button b2 = new Button() { Background = Brushes.DarkBlue };
                     b1.Click += B1_Click;
                     b1.MouseEnter += B1_MouseEnter;
                     b1.MouseLeave += B1_MouseLeave;
@@ -51,7 +58,21 @@ namespace BattleShips {
 
         private void B1_MouseLeave(object sender, MouseEventArgs e) {
             int i = player1List.IndexOf((Button)e.Source);
-            player1List[i].Background = Brushes.DarkBlue;
+            if (rotateVertical) {
+
+                if (i + a <= 99 && i + b <= 99) {
+                    player1List[i + a].Background = Brushes.DarkBlue;
+                    player1List[i + b].Background = Brushes.DarkBlue;
+                }
+
+
+
+            } else {
+                if (i + ah <= 99 && i + bh <= 99) {
+                    player1List[i + ah].Background = Brushes.DarkBlue;
+                    player1List[i + bh].Background = Brushes.DarkBlue;
+                }
+            }
         }
 
         private void B2_MouseEnter(object sender, MouseEventArgs e) {
@@ -61,18 +82,22 @@ namespace BattleShips {
         private void B1_MouseEnter(object sender, MouseEventArgs e) {
             int i = player1List.IndexOf((Button)e.Source);
             if (rotateVertical) {
-                player1List[i + 10].Background = Brushes.Yellow;
-                player1List[i + 20].Background = Brushes.Yellow;
 
+                if (i + a <= 99 && i + b <= 99) {
+                    player1List[i + a].Background = Brushes.Yellow;
+                    player1List[i + b].Background = Brushes.Yellow;
+                }
 
             } else {
-                player1List[i + 1].Background = Brushes.Yellow;
-                player1List[i + 2].Background = Brushes.Yellow;
+                if (i + ah <= 99 && i + bh <= 99) {
+                    player1List[i + ah].Background = Brushes.Yellow;
+                    player1List[i + bh].Background = Brushes.Yellow;
+                }
             }
         }
 
         private void B2_Click(object sender, RoutedEventArgs e) {
-            
+
         }
 
         private void B1_Click(object sender, RoutedEventArgs e) {
